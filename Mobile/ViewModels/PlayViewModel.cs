e5d16045063da577ee97ca54f771c8763ead34de
Marcos José <marcosjoseDV@hotmail.com>
2024-04-29 00:43:16 -0300

    IG

--- Mobile/ViewModels/PlayViewModel.cs
+++ Mobile/ViewModels/PlayViewModel.cs
@@ -1,8 +1,11 @@
 ﻿using CommunityToolkit.Maui.Views;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using DocumentFormat.OpenXml.Presentation;
+using Microsoft.Maui.Controls.PlatformConfiguration;
 using Mobile.Constans;
 using Models.Models;
+using System.Net;
 using System.Web;
 
 namespace Mobile.ViewModels;
@@ -16,7 +19,7 @@ public partial class PlayViewModel : ObservableObject, IQueryAttributable
     Assessments _assessment;
 
     [ObservableProperty]
-    string  _isBook;
+    string _isBook;
 
 
 
@@ -26,26 +29,39 @@ public partial class PlayViewModel : ObservableObject, IQueryAttributable
 
     public void ApplyQueryAttributes(IDictionary<string, object> query)
     {
+        IsBook = null;
+        Video.Source = null;
+        var teste = FileSystem.Current.AppDataDirectory;
+        Assessments assessment = (Assessments)query["Data"];
+        if (assessment.Category != "Book")
+        {
+            string fileName = Load(assessment, Configurations.ServePathMovie);
 
-        string name = (string)query["Data"];
-        Load(name);
+           Video.Source = MediaSource.FromFile(Configurations.ServePathMovie + "//" + fileName);
+        }
+        else
+        {
+            string fileName = Load(assessment, Configurations.ServePathBook);
+            IsBook = "file:///" + Configurations.ServePathBook + "//" + WebUtility.UrlEncode(fileName);
+        }
+
     }
 
-    private void Load(string name)
+    private string Load(Assessments assessment, string path)
     {
-        string decodedName = HttpUtility.UrlDecode(name);
+        string decodedName = HttpUtility.UrlDecode(assessment.Name);
 
 
-        string[] fileEntries = Directory.GetFiles(Configurations.ServePath);
-        foreach (string fileName in fileEntries)
+        string[] fileEntries = Directory.GetFiles(path);
+        foreach (string pathFile in fileEntries)
         {
-            string file = Path.GetFileNameWithoutExtension(fileName);
-            if (file.ToLower().Equals(decodedName.ToLower()))
+            string fileName = Path.GetFileNameWithoutExtension(pathFile);
+            if (fileName.Contains(decodedName, StringComparison.OrdinalIgnoreCase))
             {
-                Video.Source = MediaSource.FromFile(fileName);
-                break;
+                return fileName;
             }
         }
+        return null;
     }
 
     [RelayCommand]
